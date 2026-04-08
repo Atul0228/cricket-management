0195fd469397163946ebb59f822ee1ab240a4e0f
Atul0228 <atulyadav229000@gmail.com>
2026-04-08 14:31:26 +0530

    Add Match entity with relationship to Team

---
+++ src/main/java/com/cricket/cricket_management/model/Match.java
@@ -0,0 +1,66 @@
+package com.cricket.cricket_management.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long matchId;
+
+    private String location;
+
+    private String matchDate;
+
+    @ManyToOne
+    @JoinColumn(name = "team1_id")
+    private Team team1;
+
+    @ManyToOne
+    @JoinColumn(name = "team2_id")
+    private Team team2;
+
+    public Match() {
+    }
+
+    public Long getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(Long matchId) {
+        this.matchId = matchId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMatchDate() {
+        return matchDate;
+    }
+
+    public void setMatchDate(String matchDate) {
+        this.matchDate = matchDate;
+    }
+
+    public Team getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(Team team1) {
+        this.team1 = team1;
+    }
+
+    public Team getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(Team team2) {
+        this.team2 = team2;
+    }
+}
